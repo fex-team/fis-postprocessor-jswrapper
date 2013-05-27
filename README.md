91467c0bae7d1dc2a953b3b1361541dfa0a16dd8
zhangyunlong <zhangyunlong@baidu.com>
2013-05-27 21:04:58 +0800

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# fis-modular-jswrapper
+
+A modular plugin for fis to wrap javascript with closure or amd define
---
+++ index.js
@@ -0,0 +1,17 @@
+/*
+ * fis
+ * http://fis.baidu.com/
+ */
+
+'use strict';
+module.exports = function(content, file, conf){
+    //wrap
+    if(conf.type === 'amd'){
+//        var deps = file.requires.length ? '[\'' + file.requires.join("', '") + '\']' : '[]';
+//        content = 'define(\'' + file.getId() + '\', ' + deps + ', function(require, exports, module){\n\n' + content + '\n\n});';
+        content = 'define(\'' + file.getId() + '\', function(require, exports, module){\n\n' + content + '\n\n});';
+    } else {
+        content = '(function(){\n\n' + content + '\n\n})();';
+    }
+    return content;
+};
