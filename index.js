dff935e020f47b895f275e865e637b54cc656719
zhangyunlong <zhangyunlong@baidu.com>
2013-08-15 12:39:23 +0800

    支持手写define的文件不进行自动包装

--- index.js
+++ index.js
@@ -8,11 +8,13 @@ module.exports = function(content, file, conf){
     if(file.isMod || conf.wrapAll){
         //wrap
         if(conf.type === 'amd'){
-    //        var deps = file.requires.length ? '[\'' + file.requires.join("', '") + '\']' : '[]';
-    //        content = 'define(\'' + file.getId() + '\', ' + deps + ', function(require, exports, module){\n\n' + content + '\n\n});';
-            content = 'define(\'' + file.getId() + '\', function(require, exports, module){\n\n' + content + '\n\n});';
+            if(!/^\s*define\s*\(/.test(content)){
+                content = 'define(\'' + file.getId() + '\', function(require, exports, module){\n\n' + content + '\n\n});';
+            }
         } else {
-            content = '!function(){\n\n' + content + '\n\n}();';
+            if(!/^\s*(?:[!(]\s*|void\s+)function\(/.test(content)){
+                content = '!function(){\n\n' + content + '\n\n}();';
+            }
         }
     }
     return content;
