7a137caf5df12d876e7d7a31a923e24c349e71cb
zhangyunlong <zhangyunlong@baidu.com>
2013-09-17 11:57:11 +0800

    支持template功能

--- index.js
+++ index.js
@@ -7,12 +7,12 @@
 module.exports = function(content, file, conf){
     if(file.isMod || conf.wrapAll){
         //wrap
-        if(conf.type === 'amd'){
+        if(conf.template){
+            content = String(conf.template).split('${content}').join(content);
+        } else if(conf.type === 'amd') {
             if(!/^\s*define\s*\(/.test(content)){
                 content = 'define(\'' + file.getId() + '\', function(require, exports, module){\n\n' + content + '\n\n});';
             }
-        } else if(conf.template) {
-            content = String(conf.template).split('${content}').join(content);
         } else {
             if(!/^\s*(?:[!(]\s*|void\s+)function\(/.test(content)){
                 content = '!function(){try{\n\n' + content + '\n\n}catch(e){}}();';
